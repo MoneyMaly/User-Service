913cf489ee366e22f9a5384c5a6b2615d1c28ecc
Nofar <nofar11sh@gmail.com>
2022-04-12 17:10:24 +0300

    Add role to user

--- app/models/usermodel.py
+++ app/models/usermodel.py
@@ -6,6 +6,7 @@ from app.utils.db_helper import PyObjectId
 class User(BaseModel):
     id: Optional[PyObjectId] = Field(alias='_id')
     username: str
+    role: str
     email: Optional[str] = None
     full_name: Optional[str] = None
     disabled: Optional[bool] = None
