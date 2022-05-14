69d8d0dac44479c9a81d1ecd2e5359ed91a68773
Nofar <nofar11sh@gmail.com>
2022-05-14 16:25:19 +0300

    Delete notes

--- app/utils/auth_helper.py
+++ app/utils/auth_helper.py
@@ -31,10 +31,7 @@ async def create_access_token(data: dict, expires_time_in_minutes: int = 15):
     return encoded_jwt
 
 #endregion 
-# async def get_current_active_user(current_user: User = Depends(get_current_user)):
-#     if current_user.disabled:
-#         raise HTTPException(status_code=400, detail="Inactive user")
-#     return current_user
+
 #region verify token
 class JWTBearer(HTTPBearer):
     def __init__(self, auto_error: bool = True):
